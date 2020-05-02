f94ce69e82780bc433722318d3b6b2c0d10d3cb8
Sanjit <sanjitsanjitss30@gmail.com>
2020-05-02 20:14:34 +0530

    added CodeChef Schedule

--- contests/urls.py
+++ contests/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('CF_Schedule',views.CF_Schedule,name = 'CF_Schedule'),
-    path('',views.Contest,name = 'contest')
-    
+    path('',views.Contest,name = 'contest'),
+    path('CC_Schedule',views.CC_Schedule,name = 'CC_Schedule'),
 ]
