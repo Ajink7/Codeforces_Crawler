8c23dcfcdcaf2603bf5c464d57e263333c85f8b5
Sanjit <sanjitsanjitss30@gmail.com>
2020-05-02 17:36:35 +0530

    added CodeChef Contest Model and changed the previous CodeForcesContest model name

--- contests/models.py
+++ contests/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Contest(models.Model):
+class CF_Contest(models.Model):
     contestId = models.IntegerField()
     name = models.CharField(max_length=300)
     writers = models.CharField(max_length=300)
     starting = models.DateTimeField(null=True)
     length = models.CharField(max_length=20)
+
+class CC_Contest(models.Model):
+    contestCode = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
+    start = models.DateTimeField(null=True)
+    end = models.DateTimeField(null=True)
