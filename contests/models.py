754c4f3bc27472c68b2f2c9356c49b413bd9210b
Sanjit <sanjitsanjitss30@gmail.com>
2020-05-09 03:41:50 +0530

    updated contest list

--- contests/models.py
+++ contests/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class CF_Contest(models.Model):
-    contestId = models.IntegerField()
-    name = models.CharField(max_length=300)
-    writers = models.CharField(max_length=300)
-    starting = models.DateTimeField(null=True)
-    length = models.CharField(max_length=20)
 
-class CC_Contest(models.Model):
-    contestCode = models.CharField(max_length=100)
-    name = models.CharField(max_length=300)
-    contest_link = models.CharField(max_length=400)
-    start = models.DateTimeField(null=True)
-    end = models.DateTimeField(null=True)
 
 class Contests(models.Model):
     code = models.CharField(max_length=264,blank=True)
@@ -21,3 +9,5 @@ class Contests(models.Model):
     duration = models.CharField(max_length=264,null=True,blank=True)
     name = models.CharField(max_length=264,blank=True)
     link = models.CharField(max_length=264,blank=True)
+    platform= models.CharField(max_length=264,blank=True)
+    ending = models.DateTimeField(null=True,blank=True)
