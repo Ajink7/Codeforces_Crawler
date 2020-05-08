3ec757e8a6d904c7bf33bac28b6178861b2e7d66
Sudesh1122 <sudesh18@iitg.ac.in>
2020-05-09 04:48:11 +0530

    add ajax_filter

--- contests/urls.py
+++ contests/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 
     path('',views.Contest,name = 'contest'),
     path('ajax_update',views.ajax_update_contests,name = 'ajax_update_contest'),
-
+    path('ajax/data_filter',views.ajax_filter,name="ajax_filter"),
     path('atcoder_schedule',views.atcoder_scrape,name="atcoder_schedule"),
 ]
