d2981b81bec81645a16b4f0b1b322e93e50a458f
Sudesh1122 <sudesh18@iitg.ac.in>
2020-05-16 02:21:57 +0530

    set profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignUpView,ProfileView,UpdateProfileView,CreateProfileView
+from .views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -7,7 +7,8 @@ urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name = 'accounts/login.html' ),name= 'login'),
     path('logout/',auth_views.LogoutView.as_view(),name='logout'),
-    path('profile/',ProfileView.as_view(),name = 'profile'),
+    path('profile/<str:username>/',get_profile,name = 'profile'),
     path('create_profile/',CreateProfileView.as_view(),name = 'create_profile'),
     path('profile/update_profile/',UpdateProfileView.as_view(),name = 'update_profile')
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from . forms import UserCreateForm,UserProfileCreateForm
 from friendship.models import Friend, Follow, Block
 from django.contrib.auth.decorators import login_required,user_passes_test
+from django.contrib.auth.models import User
 
 class SignUpView(generic.CreateView):
     form_class = UserCreateForm
@@ -16,14 +17,48 @@ class SignUpView(generic.CreateView):
 
 
 
-class ProfileView(LoginRequiredMixin,generic.TemplateView):
+# class ProfileView(LoginRequiredMixin,generic.TemplateView):
 
-    template_name = 'accounts/profile.html'
+    # template_name = 'accounts/profile.html'
 
-# @login_required
+@login_required
 # @user_passes_test(condition)
-# def ProfileView(request):
-    # pass
+def get_profile(request,username):
+    user_profile = User.objects.get(username=username)
+    # List all of a user's friends:
+    friend_list = Friend.objects.friends(request.user)
+    # List all unread friendship requests:
+    friend_requests_unread = Friend.objects.unread_requests(user=request.user)
+    # List all unrejected friendship requests:
+    friend_requests = Friend.objects.unrejected_requests(user=request.user)
+    # Count of all unrejected friendship requests:
+    friend_requests_count = Friend.objects.unrejected_request_count(user=request.user)
+    # List all rejected friendship requests:
+    friend_requests_rejected = Friend.objects.rejected_requests(user=request.user)
+    # Count of all rejected friendship requests:
+    # friend_requests_rejected_count = Friend.objects.rejected_request_count(user=request.user)
+    # List of all sent friendship requests:
+    friend_requests_sent = Friend.objects.sent_requests(user=request.user)
+    # Test if two users are friends:
+    # Friend.objects.are_friends(request.user, other_user) == True
+    if request.user.username==username:
+        visitor= False
+    else:
+        visitor = True
+    context = {
+                'visitor':visitor,
+                'user_profile':user_profile,
+                'friend_list':friend_list,
+                'friend_requests_unread':friend_requests_unread,
+                'friend_requests':friend_requests,
+                'friend_requests_count':friend_requests_count,
+                'friend_requests_rejected':friend_requests_rejected,
+                # 'friend_requests_rejected_count':friend_requests_rejected_count,
+                'friend_requests_sent':friend_requests_sent,
+
+              }
+    return render(request,'accounts/profile.html',context)
+
 
 class CreateProfileView(LoginRequiredMixin,generic.UpdateView):
     model = UserProfile
